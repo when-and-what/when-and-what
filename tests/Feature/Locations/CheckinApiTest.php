850818c2a542caf014c819351dbb0a54f46a8f71
Nate Crawford <nate@whenandwhat.me>
2022-04-19 05:35:17 -0500

    ✅ add notes field to checkin api test

--- tests/Feature/Locations/CheckinApiTest.php
+++ tests/Feature/Locations/CheckinApiTest.php
@@ -12,12 +12,13 @@ class CheckinApiTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
-    public function test_checkin_with_specific_location_and_time()
+    public function test_checkin_with_specific_date()
     {
         $user = User::factory()->create();
         $data = [
             'location' => Location::factory()->create(['user_id' => $user->id])->id,
             'date' => '2022-03-01 17:00:00',
+            'note' => $this->faker->sentence(),
         ];
         $response = $this->actingAs($user)->postJson('/api/locations/checkin', $data);
         $response->assertStatus(201);
@@ -25,14 +26,16 @@ class CheckinApiTest extends TestCase
             'user_id' => $user->id,
             'location_id' => $data['location'],
             'checkin_at' => $data['date'],
+            'note' => $data['note'],
         ]);
     }
 
-    public function test_checkin_with_specific_location()
+    public function test_checkin_without_date()
     {
         $user = User::factory()->create();
         $data = [
             'location' => Location::factory()->create(['user_id' => $user->id])->id,
+            'note' => $this->faker->sentence(),
         ];
         $this->freezeTime();
         $response = $this->actingAs($user)->postJson('/api/locations/checkin', $data);
@@ -41,6 +44,7 @@ class CheckinApiTest extends TestCase
             'user_id' => $user->id,
             'location_id' => $data['location'],
             'checkin_at' => now(),
+            'note' => $data['note'],
         ]);
     }
 }
