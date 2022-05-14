9a353df152015fecfc8b4a76b5f276e5a41eaf2c
Nate Crawford <nate@whenandwhat.me>
2022-05-13 19:58:39 -0500

    🩹 missing space between attributes

--- resources/views/layouts/bootstrap.blade.php
+++ resources/views/layouts/bootstrap.blade.php
@@ -36,7 +36,7 @@
                             <a class="nav-link" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle @if(strpos($routeName, 'checkins') === 0 || strpos($routeName, 'locations') === 0) active @endif" href="#" id="navLocationDropdown"role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle @if(strpos($routeName, 'checkins') === 0 || strpos($routeName, 'locations') === 0) active @endif" href="#" id="navLocationDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Locations
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navLocationDropdown">
