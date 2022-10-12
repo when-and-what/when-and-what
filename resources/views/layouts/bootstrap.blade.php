a73862abfcdfcec816a3a828e963659b88e71725
Nate Crawford <nate@whenandwhat.me>
2022-10-11 20:06:20 -0500

    🚀 add mobile app manifest

--- resources/views/layouts/bootstrap.blade.php
+++ resources/views/layouts/bootstrap.blade.php
@@ -9,6 +9,7 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="manifest" href="{{ url('manifest.json') }}" />
 
         <!-- Styles -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
