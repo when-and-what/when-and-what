8b28f13ce3f7fa6c9a91142a6a3bb0f9e1b0422c
Nate Crawford <nate@whenandwhat.me>
2023-01-08 13:49:25 -0600

    remove old dashboard grid

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -41,17 +41,4 @@
     </div>
 </div>
 <script> var day = "{{ $today->toDateString()}}";</script>
-<div class="d-flex justify-content-between">
-    <div>
-        <h2><a href="{{ route('checkins.index') }}">Checkins</a></h2>
-        <a href="{{ route('checkins.create') }}" class="btn btn-primary">Check-in</a>
-    </div>
-    <div>
-        <h2><a href="{{ route('locations.index') }}">Locations</a></h2>
-        <a href="{{ route('locations.create') }}" class="btn btn-primary">New Location</a>
-    </div>
-    <div>
-        <h2><a href="{{ route('categories.index') }}">Categories</a></h2>
-        <a href="{{ route('categories.create') }}" class="btn btn-primary">New Category</a>
-</div>
 @endsection
