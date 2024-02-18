128865bdf0afe516a501a39f0a478fa1c320487c
Nate Crawford <nate@whenandwhat.me>
2024-02-18 09:37:24 -0600

    🐛 fix checkins at the end of the day not displaying on dashboard

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -35,8 +35,8 @@ class DashboardController extends Controller
         $start = new Carbon($date . ' 00:00:00', $request->user()->timezone);
         $end = $start->copy()->addDay();
         $checkins = Checkin::whereBelongsTo($request->user())
-            ->where('checkin_at', '>=', $start)
-            ->where('checkin_at', '<', $end)
+            ->where('checkin_at', '>=', $start->toAtomString())
+            ->where('checkin_at', '<', $end->toAtomString())
             ->with('location.category')
             ->get();
 
