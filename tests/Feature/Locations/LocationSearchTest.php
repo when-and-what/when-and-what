6b3d60ab973833cae01bae18b4e8038b192680fb
Nate Crawford <natec23@gmail.com>
2025-04-12 14:33:45 -0500

    add search form for location name

---
+++ tests/Feature/Locations/LocationSearchTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Locations\Location;
+use App\Models\User;
+
+test('example', function () {
+    $user = User::factory()->create();
+    $locations = Location::factory(5)->create(['user_id' => $user->id]);
+    $this->actingAs($user)->post(route('locations.search'), [
+        'search' => $locations[0]['name'],
+    ])
+    ->assertOk()
+    ->assertSeeText($locations[0]['name'])
+    ->assertViewHas('locations', function($locations) {
+        return count($locations) === 1;
+    });
+});
