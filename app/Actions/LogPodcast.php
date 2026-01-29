312a3eb3e6bd540835bfb33fd6e38c51ae6fb062
Nate Crawford <natec23@gmail.com>
2026-01-28 19:36:44 -0600

    📝 update model doc

---
+++ app/Actions/LogPodcast.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Podcasts\Episode;
+use App\Models\Podcasts\EpisodePlay;
+use App\Models\Podcasts\Podcast;
+use App\Models\User;
+use Illuminate\Support\Carbon;
+
+class LogPodcast
+{
+    public function fromHistory(array $history, User $user, Carbon $playDay): EpisodePlay
+    {
+        $podcast = Podcast::find($history['podcastUuid']);
+        if( !$podcast) {
+            $podcast = $this->createPodcast($history['podcastUuid'], $history['podcastTitle']);
+        }
+        $episode = Episode::find($history['uuid']);
+        if( !$episode) {
+            $episode = $this->createEpisode($history['uuid'], $history['podcastUuid'], $history['title'], $history['duration']);
+        }
+
+        $lastPlay = EpisodePlay::where('episode_uid', $history['uuid'])
+            ->where('user_id', $user->id)
+            ->orderBy('play_day', 'desc')
+            ->limit(1)
+            ->first();
+        $duration = $lastPlay ? $lastPlay->seconds : 0;
+
+        $play = new EpisodePlay;
+        $play->episode_id = $history['uuid'];
+        $play->user_id = $user->id;
+        $play->play_date = $playDay;
+        $play->seconds = $history['playedUpTo'] - $duration;
+        $play->save();
+
+        return $play;
+    }
+
+    private function createPodcast(string $uid, string $title): Podcast
+    {
+        $podcast = new Podcast;
+        $podcast->id = $uid;
+        $podcast->title = $title;
+        $podcast->save();
+
+        return $podcast;
+    }
+
+    private function createEpisode(string $uid, string $podcastUid, string $title, ?int $duration): Episode
+    {
+        $episode = new Episode;
+        $episode->id = $uid;
+        $episode->podcast_id = $podcastUid;
+        $episode->title = $title;
+        $episode->duration = $duration;
+        $episode->save();
+
+        return $episode;
+    }
+}
---
+++ app/Console/Commands/PodcastHistory.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Actions\LogPodcast;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
+use League\Flysystem\Visibility;
+
+class PodcastHistory extends Command
+{
+    protected string $filename = 'podcast_history.json';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:podcast-history';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check the pocketcast API to see what was listened to yesterday';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(LogPodcast $log)
+    {
+        $yesterday = null;
+        if(Storage::fileExists($this->filename)) {
+            $json = json_decode(Storage::get($this->filename), true);
+            $yesterday = collect($json['episodes'])->groupBy('uuid');
+        }
+        $history = Http::withToken(env('POCKETCASTS_TOKEN'))
+            ->post('https://api.pocketcasts.com/user/history')
+            ->throw()
+            ->json();
+        // Storage::put($this->filename, json_encode($history), Visibility::PRIVATE);
+
+        if($yesterday)
+        {
+            $user = User::where('email', 'natec23@gmail.com')->first();
+            foreach($history['episodes'] as $episode)
+            {
+                if(isset($yesterday[$episode['uuid']]) && $yesterday[$episode['uuid']][0]['duration'] == $episode['duration']) {
+                    return Command::SUCCESS;
+                }
+                $log->fromHistory($episode, $user, now()->yesterday());
+            }
+        }
+
+        return Command::SUCCESS;
+    }
+}
