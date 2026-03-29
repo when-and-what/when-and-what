f05407e051e8326c78b7ba9558cb0b1081c09f71
Shift <shift@laravelshift.com>
2026-03-29 19:23:03 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,17 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->validateCsrfTokens(except: [
+            '/stripe/*',
+        ]);
+
+        $middleware->web(\Laravel\Jetstream\Http\Middleware\AuthenticateSession::class);
+
+        $middleware->statefulApi();
+        $middleware->throttleApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
